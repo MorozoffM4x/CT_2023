9e352691f813875d0746328da801af3bd332e7a6
MorozoffM4x <73400058+MorozoffM4x@users.noreply.github.com>
2023-05-23 18:46:43 +0300

    DateTime parser

--- CT_2023.cpp
+++ CT_2023.cpp
@@ -1,5 +1,18 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
+#include <ctime>
+#include <time.h>
+
+#include "main.h"
+
+//#include <boost/date_time/posix_time/posix_time.hpp>
+//#include <boost/thread.hpp>
+
+const char* file_path = "DATA_Files\\CSV_Facility2Constellation\\";
+const char* file_name = "Facility-Anadyr1.csv";
+const char* SS_Time = "13 Jun 2027 23:51:33.468";
 
 using namespace std;
 
@@ -8,26 +21,118 @@ using namespace std;
 //user: u0632405_max
 //Pass: jL2wJ8wE4syH7gM1
 
+//13 Jun 2027 23:51:33.468    14 Jun 2027 00:00:01.000     507.532
+
 
 int main()
 {
-    string filename = "DATA_Files\\Facility2Constellation\\Facility-Anadyr1.txt";
-    ifstream f;
+    char result[100];
+    strcpy_s(result, file_path);
+    strcat_s(result, file_name);
+    //const char* all_path = strcat(file_path, file_name);
+    //file_name = "DATA_Files\\CSV_Facility2Constellation\\Facility-Anadyr1.csv";
 
-    f.open(filename);
+    //ifstream f("DATA_Files\\Facility2Constellation\\Facility-Anadyr1.txt");
+    //string s;
+    //for (int c = 1; getline(f, s) && c <= 100; ++c) {
+    //    if (c > 10 && c <= 100) cout << s << std::endl;
+    //}
+    //f.close();
 
-    if (!f.is_open()) {
-        return 1;
-    }
-    while (f) {
-        string line;
-        
-        f >> line;
-        const char* c = line.c_str();
-        printf(c);
- 
-    }
+    //reader_csv(result);
+    cout << get_date_time(SS_Time);
 
-    f.close();
     return 0;
+}
+
+int reader_csv(const char* file_name)
+
+{
+    string fi = file_name;
+    ifstream work_file(fi);
+    string line;
+    char delimiter = ',';
+
+    getline(work_file, line);
+    // Прочитали все строчки
+    while (getline(work_file, line))
+    {
+        stringstream stream(line); // Преобразование строки в поток
+        string Iter, Name, Access, Start_Time, Stop_Time, Duration;
+        
+        // Извлечение всех значений в этой строке
+        getline(stream, Iter, delimiter);
+        getline(stream, Name, delimiter);
+        getline(stream, Access, delimiter);
+        getline(stream, Start_Time, delimiter);
+        getline(stream, Stop_Time, delimiter);
+        getline(stream, Duration, delimiter);
+
+
+
+        //cout << "Iter: " << Iter << "  Name: " << Name << "   Access num: " << Access << "   Start: " << Start_Time << "   Stop: " << Stop_Time << "   Duration: " << Duration << endl;
+        cout << Iter << "   " << Name << "  " << Access << "    " << Start_Time << "    " << Stop_Time << "     " << Duration << endl;
+
+    }
+    work_file.close();
+    return 0;
+    
+}
+
+char get_date_time(const char* SS_Time)
+{
+    const char* m = SS_Time;
+    char s;
+    string day, month, year, hour, min, sec, millis;
+    //printf(SS_Time);
+
+    for (int a = 0; a != 2; a++) 
+    {
+        //s = a[m];
+        day.append(1, m[a]);
+    }
+    for (int a = 3; a != 6; a++)
+    {
+        //s = a[m];
+        month.append(1, m[a]);
+    }
+    for (int a = 7; a != 11; a++)
+    {
+        //s = a[m];
+        year.append(1, m[a]);
+    }
+    for (int a = 12; a != 14; a++)
+    {
+        //s = a[m];
+        hour.append(1, m[a]);
+    }
+    for (int a = 15; a != 17; a++)
+    {
+        //s = a[m];
+        min.append(1, m[a]);
+    }
+    for (int a = 18; a != 20; a++)
+    {
+        //s = a[m];
+        sec.append(1, m[a]);
+    }
+    for (int a = 21; a != 24; a++)
+    {
+        //s = a[m];
+        millis.append(1, m[a]);
+    }
+    cout << day << " ";
+    cout << month << " ";
+    cout << year << " ";
+    cout << hour << " ";
+    cout << min << " ";
+    cout << sec << " ";
+    cout << millis;
+    //printf(dt);
+    //printf(a[1]);
+    //struct std::tm when;
+    //std::time(s);
+    //string s = ;
+    return 0;
+
 }
---
+++ main.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int reader_csv(const char* file_name);
+char get_date_time(const char* SS_Time);
