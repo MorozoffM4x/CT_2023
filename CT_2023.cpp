c3c7fd7208c87b817d8d59ac8d76423bcfccdd2b
MorozoffM4x <73400058+MorozoffM4x@users.noreply.github.com>
2023-05-24 17:31:54 +0300

    Расчет времени и вывод разницы

--- CT_2023.cpp
+++ CT_2023.cpp
@@ -6,19 +6,21 @@
 #include <time.h>
 #include <iomanip>
 #include <map>
+#include <boost/filesystem.hpp>
+#include "boost/date_time/posix_time/posix_time.hpp"
+#include <boost/format.hpp>
+
+using namespace boost::filesystem;
+using namespace boost::posix_time;
+using namespace std;
 
 #include "main.h"
 #include <format>
 
-
-//#include <boost/date_time/posix_time/posix_time.hpp>
-//#include <boost/thread.hpp>
-
 const char* file_path = "DATA_Files\\CSV_Facility2Constellation\\";
 const char* file_name = "Facility-Anadyr1.csv";
 const char* SS_Time = "13 Jun 2027 23:51:33.468";
-
-using namespace std;
+const char* SS2_Time = "14 Jun 2027 00:00:01.000";
 
 //addr: maxodrom.ru (31.31.196.25)
 //base: u0632405_CT_2023
@@ -33,6 +35,7 @@ int main()
     char result[100];
     strcpy_s(result, file_path);
     strcat_s(result, file_name);
+
     //const char* all_path = strcat(file_path, file_name);
     //file_name = "DATA_Files\\CSV_Facility2Constellation\\Facility-Anadyr1.csv";
 
@@ -43,11 +46,25 @@ int main()
     //}
     //f.close();
 
-    //reader_csv(result);
+    //reader_csv(result); //чтение csv
+    cout << get_date_time(SS_Time)<<endl; //Получение строки нужного формата с датой и временем
 
+    boost::posix_time::time_duration diff;
+    
+    ptime xTimeStr(time_from_string(get_date_time(SS_Time)));
+    ptime xTimeStr2(time_from_string(get_date_time(SS2_Time)));
+    cout << xTimeStr << endl;
+    cout << xTimeStr2 << endl;
+    diff = xTimeStr2 - xTimeStr;
 
+    cout << diff << endl; //Выводит разницу времени
 
-    cout << get_date_time(SS_Time);
+    long milliseconds = diff.total_milliseconds();
+    cout << milliseconds << endl; //Выводит разницу времени в милисекундах
+
+    boost::format output("%.3f");
+    output % (milliseconds / 1000.0);
+    std::cout << output << std::endl; //Выводит разницу времени в секундах
 
     return 0;
 }
@@ -75,8 +92,6 @@ int reader_csv(const char* file_name)
         getline(stream, Stop_Time, delimiter);
         getline(stream, Duration, delimiter);
 
-
-
         //cout << "Iter: " << Iter << "  Name: " << Name << "   Access num: " << Access << "   Start: " << Start_Time << "   Stop: " << Stop_Time << "   Duration: " << Duration << endl;
         cout << Iter << "   " << Name << "  " << Access << "    " << Start_Time << "    " << Stop_Time << "     " << Duration << endl;
 
@@ -86,75 +101,50 @@ int reader_csv(const char* file_name)
     
 }
 
-char get_date_time(const char* SS_Time)
+string get_date_time(const char* SS_Time)
 {
     const char* m = SS_Time;
     char s;
-    string day, month, year, hour, min, sec, millis;
+    string day, month, year, date_time_final, mnth, time;
 
     map<string, int> months{
         {"Jan", 1}, {"Feb", 2}, {"Mar", 3}, {"Apr", 4}, {"May", 5}, {"Jun", 6}, {"Jul", 7}, {"Aug", 8}, {"Sep", 9}, {"Oct", 10}, {"Nov", 11}, {"Dec", 12}
     };
-    //format("a = {:02}", a)
+    
     for (int a = 0; a != 2; a++) 
     {
-        //s = a[m];
         day.append(1, m[a]);
     }
     for (int a = 3; a != 6; a++)
     {
-        //s = a[m];
         month.append(1, m[a]);
-        
     }
-    for (int a = 7; a != 11; a++)
-    {
-        //s = a[m];
-        year.append(1, m[a]);
-    }
-    for (int a = 12; a != 14; a++)
-    {
-        //s = a[m];
-        hour.append(1, m[a]);
-    }
-    for (int a = 15; a != 17; a++)
-    {
-        //s = a[m];
-        min.append(1, m[a]);
-    }
-    for (int a = 18; a != 20; a++)
-    {
-        //s = a[m];
-        sec.append(1, m[a]);
-    }
-    for (int a = 21; a != 24; a++)
-    {
-        //s = a[m];
-        millis.append(1, m[a]);
-    }
-    
-    cout << day << " ";
     if (months[month] < 10)
     {
-        cout << 0 << months[month] << " ";
+        mnth = "0";
+        mnth.append(to_string(months[month]));
     }
-    else 
-        cout << months[month] << " ";
-    //cout << setw(2) << setfill('0') << month_e << " ";
+    else
+        mnth = to_string(months[month]);
 
-    cout << year << " ";
-    cout << hour << " ";
-    cout << min << " ";
-    cout << sec << " ";
-    cout << millis;
+    for (int a = 7; a != 11; a++)
+    {
+        year.append(1, m[a]);
+    }
+    for (int a = 12; a != 24; a++)
+    {
+        time.append(1, m[a]);
+    }
 
-    //printf(dt);
-    //printf(a[1]);
-    //struct std::tm when;
-    //std::time(s);
-    //string s = ;
+    date_time_final.append(year);
+    date_time_final.append("-");
+    date_time_final.append(mnth);
+    date_time_final.append("-");
+    date_time_final.append(day);
+    date_time_final.append(" ");
+    date_time_final.append(time);
 
-    return 0;
+    return date_time_final;
 
 }
 
--- main.h
+++ main.h
@@ -1,4 +1,4 @@
 #pragma once
 
 int reader_csv(const char* file_name);
-char get_date_time(const char* SS_Time);
+string get_date_time(const char* SS_Time);
