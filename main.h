cf7f25899cf489743fd202ee2ac346e8878540e4
MorozoffM4x <73400058+MorozoffM4x@users.noreply.github.com>
2023-05-26 00:04:56 +0300

    Добавил примерный план

--- main.h
+++ main.h
@@ -3,3 +3,4 @@
 int reader_csv(const char* file_name);
 string get_date_time(const char* SS_Time);
 int merge_files(const char* file_path);
+string get_d_t(const char* d_time1, const char* d_time2);
